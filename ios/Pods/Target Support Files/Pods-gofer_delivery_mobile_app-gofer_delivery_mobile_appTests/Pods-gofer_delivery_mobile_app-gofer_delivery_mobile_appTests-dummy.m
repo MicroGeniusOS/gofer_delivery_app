c5da0db0796357ea9b570d950f5802562d269c46
Abdul Rahman Mohammed <30295447+rahmangate@users.noreply.github.com>
2021-06-19 13:42:38 +0000

    intial commit

---
+++ ios/Pods/Target Support Files/Pods-gofer_delivery_mobile_app-gofer_delivery_mobile_appTests/Pods-gofer_delivery_mobile_app-gofer_delivery_mobile_appTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_gofer_delivery_mobile_app_gofer_delivery_mobile_appTests : NSObject
+@end
+@implementation PodsDummy_Pods_gofer_delivery_mobile_app_gofer_delivery_mobile_appTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-gofer_delivery_mobile_app/Pods-gofer_delivery_mobile_app-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_gofer_delivery_mobile_app : NSObject
+@end
+@implementation PodsDummy_Pods_gofer_delivery_mobile_app
+@end
